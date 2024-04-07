7a5e52fbbb7b3d30512a90c03561cfd99122f300
mert-celik-1 <mertcelik2399@gmail.com>
2024-04-07 10:13:40 +0300

    thread

--- 1-Thread-Async/Program.cs
+++ 1-Thread-Async/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿X();
+Y();
 
-Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+async void X()
+{
+    await Task.Run(() =>
+    {
+        for (int i = 0; i < 999; i++)
+            Console.WriteLine($"X - {i}");
+    });
+}
+async void Y()
+{
+    await Task.Run(() =>
+    {
+        for (int i = 0; i < 999; i++)
+            Console.WriteLine($"Y - {i}");
+    });
+}
