562a3c2f5c10615afc9ecabf83788bcdf8938614
mert-celik-1 <mertcelik2399@gmail.com>
2024-04-09 02:57:57 +0300

    Interlocked

--- 2-Thread/obj/Debug/net8.0/2-Thread.AssemblyInfo.cs
+++ 2-Thread/obj/Debug/net8.0/2-Thread.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2-Thread")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92fc5b0098408365f2929a5468ec0d453654cdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd16aeab84f1c653ff183a159a8c6b9288ec8d19")]
 [assembly: System.Reflection.AssemblyProductAttribute("2-Thread")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2-Thread")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 6-Thread-Sync-Interlocked/Program.cs
+++ 6-Thread-Sync-Interlocked/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region Interlocked Sınıfı
+//int i = 0;
 
-Console.WriteLine("Hello, World!");
+//var prevalue = Interlocked.Exchange(ref i, 5);
+//Console.WriteLine(i);
+//Console.WriteLine(prevalue);
+
+//Interlocked.CompareExchange(ref i, 15, 5);
+//Console.WriteLine(i);
+
+//Thread thread1 = new(() =>
+//{
+//    while (true)
+//        //i++;
+//        Interlocked.Increment(ref i);
+//});
+//Thread thread2 = new(() =>
+//{
+//    while (true)
+//        Console.WriteLine(i);
+//});
+//Thread thread3 = new(() =>
+//{
+//    while (true)
+//        //i--;
+//        Interlocked.Decrement(ref i);
+//});
+
+//thread1.Start();
+//thread2.Start();
+//thread3.Start();
+#endregion
+#region MemoryBarrier Metodu
+int i = 0;
+Thread writeThread = new(() =>
+{
+    while (true)
+    {
+        Interlocked.Increment(ref i);
+        Thread.MemoryBarrier();
+    }
+});
+Thread readThread = new(() =>
+{
+    while (true)
+    {
+        Thread.MemoryBarrier();
+        Console.WriteLine(i);
+    }
+});
+
+writeThread.Start();
+readThread.Start();
+#endregion
